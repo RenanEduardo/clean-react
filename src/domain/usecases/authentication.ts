de68d65be23bb11abe512d8ea38388b4be69f239
Renan Eduardo F. da Silva <renaneduardo1@hotmail.com>
2021-07-17 17:47:49 -0300

    chore: remove files

--- src/domain/usecases/authentication.ts
+++
@@ -1,9 +0,0 @@
-import { AccountModel } from '../models/account-model'
-
-type AuthenticatinParams = {
-  email: string
-  password: string
-}
-export interface Authentication {
-  auth: (params: AuthenticatinParams) => Promise<AccountModel>
-}
