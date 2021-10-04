9db8a1111dde68d60164883aae5977dbefcb4833
Renan Eduardo F. da Silva <renaneduardo1@hotmail.com>
2021-10-04 17:09:38 -0300

    fix: remove empty space from id

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -7,7 +7,7 @@ const FormStatus: React.FC = () => {
   const { state } = useContext(Context)
   const { isLoading, mainError } = state
   return (
-    <div data-testid="error-wrap "className={Styles.errorWrap}>
+    <div data-testid="error-wrap"className={Styles.errorWrap}>
       {isLoading && <Spinner className={Styles.spinner}/>}
       {mainError && <span data-testid="main-error" className={Styles.error}>{mainError}</span>}
     </div>
