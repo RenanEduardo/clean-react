b4c8297e5e102687f9188fc481e6d684fde072f1
Renan Eduardo F. da Silva <renaneduardo1@hotmail.com>
2021-10-24 22:12:19 -0300

    feat: add skeleton shimmer effect

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -4,51 +4,28 @@ import Styles from './survey-list-styles.scss'
 
 const SurveyList: React.FC = () => {
   return (
-   <div className={Styles.surveyListWrap}>
-     <Header />
-     <div className={Styles.contentWrap}>
-       <h2>Enquetes</h2>
-       <ul>
-         <li>
-           <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp}/>
+    <div className={Styles.surveyListWrap}>
+      <Header />
+      <div className={Styles.contentWrap}>
+        <h2>Enquetes</h2>
+        <ul>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
               <time>
                 <span className={Styles.day}>24</span>
                 <span className={Styles.month}>10</span>
                 <span className={Styles.year}>2021</span>
               </time>
               <p>Qual é seu framework web favorito?</p>
-           </div>
-           <footer>Ver Resultado</footer>
-         </li>
-         <li>
-           <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp}/>
-              <time>
-                <span className={Styles.day}>24</span>
-                <span className={Styles.month}>10</span>
-                <span className={Styles.year}>2021</span>
-              </time>
-              <p>Qual é seu framework web favorito?</p>
-           </div>
-           <footer>Ver Resultado</footer>
-         </li>
-         <li>
-           <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp}/>
-              <time>
-                <span className={Styles.day}>24</span>
-                <span className={Styles.month}>10</span>
-                <span className={Styles.year}>2021</span>
-              </time>
-              <p>Qual é seu framework web favorito?</p>
-           </div>
-           <footer>Ver Resultado</footer>
-         </li>
-       </ul>
-     </div>
-     <Footer />
-   </div>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li></li>
+        </ul>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
